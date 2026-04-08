48d2a6c0b6fa03c09969d4c7431ed1225b2ae531
ljj <1412415464@qq.com>
2026-04-08 17:34:00 +0800

    搭建代码框架

---
+++ src/test/java/org/example/javabackendnotes/JavaBackendNotesApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.javabackendnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendNotesApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}

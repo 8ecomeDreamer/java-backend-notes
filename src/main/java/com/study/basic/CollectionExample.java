f9e8022ea2cff7ce2565a9af3726f41c36c855df
ljj <1412415464@qq.com>
2026-05-03 17:10:07 +0800

    java查漏补缺：Exception的学习。

---
+++ src/main/java/com/study/basic/CollectionExample.java
@@ -0,0 +1,20 @@
+package com.study.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionExample {
+
+    public static void collectionIntroduce() {
+        System.out.println();
+        System.out.println("1.Collection：List、Set、Queue");
+        System.out.println("List: 有序、可重复的集合，代表有ArrayList和linkedList");
+        System.out.println("Set: 无序、不可重复的集合，代表有HashSet");
+        System.out.println("Queue: 队列");
+        System.out.println("2.Map：键值对集合，代表有HashMap");
+    }
+
+    public static void main(String[] args) {
+        collectionIntroduce();
+    }
+}
